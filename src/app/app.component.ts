295cb8a5ad1e9d1a7c521e4b81c465743ac1bc77
JEFFERSON FELIPE ALVES DA SILVA <a9261848@rn.senac.br>
2019-10-09 21:22:47 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+class Contato {
+  id: number;
+  nome: string;
+  fone: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'agenda-front';
+
+  ultimoId = 3;
+  contato = new Contato();
+
+  contatos = [];
+
+  salvar(formContato: NgForm) {
+    this.contato.id = ++this.ultimoId;
+    this.contato.nome = formContato.value.nome;
+    this.contato.fone = formContato.value.fone;
+
+    this.contatos.push(this.contato);
+
+    this.contato = new Contato();
+  }
+
+  editar(id: number) {
+    for (const c of this.contatos) {
+      console.log(c);
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import {ContatoService} from './contato.service';
+
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    ContatoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
